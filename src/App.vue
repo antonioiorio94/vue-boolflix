2fba8519ed1bd7394c19507abe3fb639c330e28f
Antonio Iorio <antonioiorio94@gmail.com>
2022-04-20 15:33:36 +0200

    aggiunta chiamata axios + console.log risultato

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,35 @@
 
 <script>
 import BoolflixHeader from "./components/BoolflixHeader.vue";
+import axios from "axios";
 
 export default {
   name: "App",
   components: {
     BoolflixHeader,
+  },
+  data() {
+    return {
+      apiUrl: "https://api.themoviedb.org/3/search/",
+      apiKey: "728f321f4e1478d4908741def6f96176",
+    };
+  },
+  mounted() {
+    const query = "ritorno";
+    const params = {
+      query,
+      api_key: this.apiKey,
+      language: "it-IT",
+    };
+
+    axios
+      .get(this.apiUrl + "tv", { params })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
 };
 </script>

9b8d968c3c9b2956273fdb3c47e769b00744a8b2
Antonio Iorio <antonioiorio94@gmail.com>
2022-04-21 17:04:16 +0200

    aggiunta effetto hover e file scss separato

--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -13,10 +13,19 @@
             :src="imgUrl + movie.poster_path"
             :alt="movie.title"
           />
-          <h5>{{ movie.title }}</h5>
-          <p>Titolo originale: {{ movie.original_title }}</p>
-          <p class="text-uppercase">Lingua: {{ movie.original_language }}</p>
-          <p>Voto: {{ movie.vote_average }}</p>
+          <div class="card-body">
+            <h5>
+              <span class="fw-bold">{{ movie.title }}</span>
+            </h5>
+            <p>
+              <span class="fw-bold">Titolo originale: </span
+              >{{ movie.original_title }}
+            </p>
+            <p class="text-uppercase">
+              <span class="fw-bold">Lingua: </span>{{ movie.original_language }}
+            </p>
+            <p><span class="fw-bold">Voto: </span>{{ movie.vote_average }}</p>
+          </div>
         </div>
       </div>
     </div>
@@ -34,26 +43,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.row {
-  text-align: -webkit-center;
-  padding: 20px;
-  h2 {
-    text-align: start;
-    font-weight: bold;
-    color: white;
-    margin-bottom: 1rem;
-    font-size: 1.5rem;
-  }
-  .card {
-    padding: 20px;
-    background-color: transparent;
-    text-align: -webkit-center;
-    margin-bottom: 2rem;
-    border: 1px solid white;
-    height: 100%;
-    h5 {
-      font-weight: bold;
-    }
-  }
-}
+@import "@/style/card-style";
 </style>
--- src/components/SeriesList.vue
+++ src/components/SeriesList.vue
@@ -13,10 +13,12 @@
             :src="imgUrl + serie.poster_path"
             :alt="serie.name"
           />
-          <h5>{{ serie.name }}</h5>
-          <p>Titolo originale: {{ serie.original_name }}</p>
-          <p class="text-uppercase">Lingua: {{ serie.original_language }}</p>
-          <p>Voto: {{ serie.vote_average }}</p>
+          <div class="card-body">
+            <h5>{{ serie.name }}</h5>
+            <p>Titolo originale: {{ serie.original_name }}</p>
+            <p class="text-uppercase">Lingua: {{ serie.original_language }}</p>
+            <p>Voto: {{ serie.vote_average }}</p>
+          </div>
         </div>
       </div>
     </div>
@@ -34,26 +36,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.row {
-  text-align: -webkit-center;
-  padding: 20px;
-  h2 {
-    text-align: start;
-    font-weight: bold;
-    color: white;
-    margin-bottom: 1rem;
-    font-size: 1.5rem;
-  }
-  .card {
-    padding: 20px;
-    background-color: transparent;
-    text-align: -webkit-center;
-    margin-bottom: 2rem;
-    border: 1px solid white;
-    height: 100%;
-    h5 {
-      font-weight: bold;
-    }
-  }
-}
+@import "@/style/card-style";
 </style>

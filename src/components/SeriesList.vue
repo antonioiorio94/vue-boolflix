9e9575ac1602c9ca96706f38f46cae5d16c2ad8e
Antonio Iorio <antonioiorio94@gmail.com>
2022-04-22 16:26:42 +0200

    prova aggiunta stelle

--- src/components/SeriesList.vue
+++ src/components/SeriesList.vue
@@ -25,7 +25,11 @@
             <h5>{{ serie.name }}</h5>
             <p>Titolo originale: {{ serie.original_name }}</p>
             <p class="text-uppercase">Lingua: {{ serie.original_language }}</p>
-            <p>Voto: {{ serie.vote_average }}</p>
+            <div>
+              <p>Voto: {{ averageVote(serie.vote_average) }}</p>
+              <i v-for="i in vote" :key="i" class="fa fa-2x fa-star"></i>
+            </div>
+
             <p><span class="fw-bold">Riassunto: </span>{{ serie.overview }}</p>
           </div>
         </div>
@@ -37,9 +41,22 @@
 <script>
 export default {
   name: "SeriesList",
+  data() {
+    return {
+      vote: [],
+    };
+  },
   props: {
     seriesList: Array,
     imgUrl: String,
+  },
+  methods: {
+    //TRASFORMO IL VOTO DECIMALE IN UN VOTO DA 0 A 5
+    averageVote(n) {
+      this.vote = Math.ceil(n / 2);
+      console.log(this.vote);
+      return this.vote;
+    },
   },
 };
 </script>
